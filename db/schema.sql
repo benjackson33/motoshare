e3188e58c4f30a39a731b0bef7f4c727cbaf11d0
Ben Jackson <benjacksonsmac@gmail.com>
2023-06-29 22:45:53 +1200

    css updates

--- db/schema.sql
+++ db/schema.sql
@@ -49,14 +49,26 @@ VALUES ('background', 'https://kickstart.bikeexif.com/wp-content/uploads/2017/09
 
 update bikes set user_id = 1;
 
-INSERT INTO bikes (title, brand, model, category, image_url)
-VALUES ('Triumph Scrambler', 'Triumph', 'Street Twin', 'Scrambler', 'https://m.atcdn.co.uk/ect/media/w1224/28328d444508467f82c42713bca78db8.jpg');
+
 
 INSERT INTO bikes (title, brand, model, category, image_url)
 VALUES ('Artisan Bobber', 'BMW', 'R80/7', 'Bobber', 'https://kevilsspeedshop.com/bmw/wp-content/uploads/2014/02/F34A1153-708x472.jpg');
 
 INSERT INTO bikes (title, brand, model, category, image_url)
 VALUES ('OJO Cafe Racer', 'BMW', 'R100RS', 'Cafe Racer', 'https://kevilsspeedshop.com/bmw/wp-content/uploads/2018/11/UNADJUSTEDNONRAW_thumb_f6ff-708x531.jpg');
+
+
+
+
+
+INSERT INTO bikes (title, brand, model, category, image_url)
+VALUES ('Street Scrambler', 'BMW', 'R100GS', 'Scrambler', 'https://kevilsspeedshop.com/bmw/wp-content/uploads/2019/10/17F8ED2C-18E0-4F9B-A92F-D153068025F1-708x531.jpeg');
+
+INSERT INTO bikes (title, brand, model, category, image_url)
+VALUES ('Primera Tracker', 'Honda', 'NX650', 'Tracker', 'http://siderockcycles.com/bikes/primeratracker/9.jpg');
+
+INSERT INTO bikes (title, brand, model, category, image_url)
+VALUES ('Aquila Nera ', 'Moto Guzzi', 'Le Mans 1000', 'Cafe Racer', 'http://siderockcycles.com/bikes/aquilanera/4.jpg');
 
 
 
